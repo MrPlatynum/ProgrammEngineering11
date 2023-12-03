3790c5e3bc3b437ef5b33f3fb660db084fb29616
MrPlatynum <dushinsasha4@gmail.com>
2023-12-04 00:32:10 +0300

    edit individual1.py

--- individual1.py
+++ individual1.py
@@ -1,76 +1,84 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+def add_train(trains):
+    """Добавляет информацию о поезде."""
+    destination = input("Название пункта назначения: ")
+    train_number = input("Номер поезда: ")
+    departure_time = input("Время отправления (в формате ЧЧ:ММ): ")
 
-def get_trains_info():
-    """
-    Функция запрашивает у пользователя информацию о поездах и возвращает список словарей поездов.
-    """
-    num_trains = int(input("Введите количество поездов: "))
-    trains = []
+    train = {
+        'название пункта назначения': destination,
+        'номер поезда': train_number,
+        'время отправления': departure_time,
+    }
 
-    for _ in range(num_trains):
-        destination = input("Введите название пункта назначения: ")
-        train_number = input("Введите номер поезда: ")
-        departure_time = input("Введите время отправления (в формате HH:MM): ")
-
-        train = {
-            'название пункта назначения': destination,
-            'номер поезда': train_number,
-            'время отправления': departure_time
-        }
-        trains.append(train)
-
-    return trains
+    trains.append(train)
+    trains.sort(key=lambda x: x['название пункта назначения'])
 
 
-def sort_trains(trains):
-    """
-    Функция сортирует список поездов по названию пункта назначения.
-    """
-    return sorted(trains, key=lambda x: x['название пункта назначения'])
+def list_trains(trains):
+    """Выводит список всех поездов."""
+    line = f'+-{"-" * 35}-+-{"-" * 15}-+-{"-" * 25}-+'
+    print(line)
+    print(f"| {'Название пункта назначения':^35} | {'Номер поезда':^15} | {'Время отправления':^25} |")
+
+    for train in trains:
+        print(line)
+        print(
+            f"| {train['название пункта назначения']:^35} | {train['номер поезда']:^15} | {train['время отправления']:^25} |")
+    print(line)
 
 
-def display_sorted_trains(sorted_trains):
-    """
-    Функция выводит отсортированный список поездов на экран.
-    """
-    print("\nСписок поездов:")
-    for train in sorted_trains:
-        print(train)
+def select_trains(trains, search_time):
+    """Выводит поезда, отправляющиеся после указанного времени."""
+    found = False
+    result = []
+
+    print(f"Поезда, отправляющиеся после {search_time}:")
+    for train in trains:
+        train_time = train['время отправления']
+        if train_time >= search_time:
+            result.append(train)
+            found = True
+    if found:
+        return result
+
+    if not found:
+        return "Нет поездов, отправляющихся после указанного времени."
 
 
-def search_trains_by_time(sorted_trains):
-    """
-    Функция выполняет поиск поездов, отправляющихся после определенного времени.
-    """
-    search_time = input("\nВведите время для поиска поездов (в формате HH:MM): ")
-    found_trains = [train for train in sorted_trains if train['время отправления'] > search_time]
-    return found_trains
-
-
-def display_found_trains(found_trains, search_time):
-    """
-    Функция выводит найденные поезда, отправляющиеся после определенного времени, на экран.
-    """
-    if found_trains:
-        print("\nПоезда, отправляющиеся после {}: ".format(search_time))
-        for train in found_trains:
-            print(train)
-    else:
-        print("\nНет поездов, отправляющихся после {}".format(search_time))
+def display_help():
+    """Выводит справку о доступных командах."""
+    print("Список команд:\n")
+    print("add - добавить информацию о поезде;")
+    print("list - вывести список всех поездов;")
+    print("select <время> - вывести поезда, отправляющиеся после указанного времени;")
+    print("exit - завершить работу с программой.")
 
 
 def main():
-    """
-    Главная функция программы.
-    """
-    trains = get_trains_info()
-    sorted_trains = sort_trains(trains)
-    display_sorted_trains(sorted_trains)
-    found_trains = search_trains_by_time(sorted_trains)
-    display_found_trains(found_trains, search_time=input("Введите время для поиска: "))
+    """Основная функция управления программой."""
+    trains = []
+
+    while True:
+        command = input(">>> ").lower()
+
+        match command:
+            case 'exit':
+                break
+            case 'add':
+                add_train(trains)
+            case 'list':
+                list_trains(trains)
+            case 'select':
+                selected = select_trains(trains, input("Введите время для поиска поездов (в формате ЧЧ:ММ): "))
+                list_trains(selected)
+            case 'help':
+                display_help()
+            case _:
+                print(f"Неизвестная команда {command}")
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
